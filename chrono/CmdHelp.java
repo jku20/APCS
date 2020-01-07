ed8f8fffea5b768bd9bededf8509c2a9b2a675dd
jku20 <jku-benjet20@stuy.edu>
2020-01-06 21:29:04 -0500

    A bit of reorganizing, making command separate objects

---
+++ chrono/CmdHelp.java
@@ -0,0 +1,17 @@
+class CmdHelp implements Command {
+    private String[] cmdList;
+    public CmdHelp(String[] cmdList) {
+        this.cmdList = cmdList;
+    }
+    public void info() {
+        System.out.println(
+                "
+                ----help----
+                help prints out all current commands
+                ");
+    }
+    public void run(){
+        for(String cmd : cmdList)
+            System.out.println(cmd);
+    }
+}
---
+++ chrono/Command.java
@@ -0,0 +1,4 @@
+interface Command {
+    public void info(); //description of command function printed to cmd ln
+    public void run(); //runs the command
+}
