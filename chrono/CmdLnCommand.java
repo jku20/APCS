4434a28bbd73051119fc6d37c35e1484bad6f23c
jku20 <jku-benjet20@stuy.edu>
2020-01-07 21:16:30 -0500

    A little extra help stuff, so that works now pretty much

--- chrono/CmdLnCommand.java
+++ chrono/CmdLnCommand.java
@@ -9,21 +9,42 @@ class CmdLnCommand {
     //this returns the code which the command will be. It is just and integer which will count up for more command names as needed.
     public void nextCommand() {
         String command = this.sc.nextLine();
-        switch(command) {
-            case "help":
-                Command help = new CmdHelp(new String[] {"tasks", "help", "quit"});
-                help.run();
-                break;
-            case "tasks":
-                System.out.println("implement tasks function");
-                break;
-            case "quit":
-                Command quit = new CmdQuit();
-                quit.run();
-                break;
-            case "save":
-                System.out.println("implement save function in CSV writer class thing");
-                break;
+        //two cases, keyword help which gets description of command
+        //and just regualar command runnin
+        if(!command.split(" ")[0].equals("help") || command.split(" ").length == 1) {
+            switch(command) {
+                case "help":
+                    Command help = new CmdHelp(new String[] {"tasks", "help", "quit"});
+                    help.run();
+                    break;
+                case "tasks":
+                    System.out.println("implement tasks function");
+                    break;
+                case "quit":
+                    Command quit = new CmdQuit();
+                    quit.run();
+                    break;
+                case "save":
+                    System.out.println("implement save function CSV writer class thing");
+                    break;
+            }
+        } else {
+            switch(command.split(" ")[1]) {
+                case "help":
+                    Command help = new CmdHelp(new String[] {"tasks", "help", "quit"});
+                    help.info();
+                    break;
+                case "tasks":
+                    System.out.println("implement tasks funciton");
+                    break;
+                case "quit":
+                    Command quit = new CmdQuit();
+                    quit.info();
+                    break;
+                case "save":
+                    System.out.println("implement save fuction CSV writer class thing");
+                    break;
+            }
         }
     }
 
