ce376fa508d940ed1b1f47d1db924d06dba058a4
jku20 <jku-benjet20@stuy.edu>
2020-01-19 23:55:06 -0500

    initial tasks file

--- chrono/Task.java
+++ chrono/Task.java
@@ -1,5 +1,26 @@
 class Task {
-    int start;
-    int end;
+    long start;
+    long end;
     String desc;
-
+    public Task(String desc) {
+        this.desc = desc;
+    }
+    public Task() {
+        this("Undescribable");
+    }
+    public void startTimer() {
+        start = System.currentTimeMillis() / 1000L;
+    }
+    public void stopTimer() {
+        end = System.currentTimeMillis() / 1000L;
+    }
+    public int getStart() {
+        return toIntExact(start);
+    }
+    public int getEnd() {
+        return toIntExact(end);
+    }
+    public int getDuration() {
+        return getEnd() - getStart();
+    }
+}
